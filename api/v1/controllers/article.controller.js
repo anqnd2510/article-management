aae0a5485d0a130ff5d28308bae229000d32b6e2
anqnd2510 <duyan251003@gmail.com>
2024-06-16 22:22:45 +0700

    filter status for article

--- api/v1/controllers/article.controller.js
+++ api/v1/controllers/article.controller.js
@@ -1,17 +1,23 @@
 const Article = require("../../../models/article.model");
 
+// [GET]/api/v1/articles
 module.exports.index = async (req, res) => {
-    const articles = await Article.find({
+    const find = {
         deleted: false
-    })
+    };
+    if(req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const articles = await Article.find(find);
     
     res.json(articles);
 };
 
+// [GET]/api/v1/articles/detail/:id
 module.exports.detail = async (req, res) => {
     try {
         const id = req.params.id;
-
         const article = await Article.findOne({
             _id: id,
             deleted: false
