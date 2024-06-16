a3dc49a7dc194c3cf5074d1663d77df8234fe9a2
anqnd2510 <duyan251003@gmail.com>
2024-06-16 22:44:26 +0700

    function sort for article

--- api/v1/controllers/article.controller.js
+++ api/v1/controllers/article.controller.js
@@ -7,9 +7,19 @@ module.exports.index = async (req, res) => {
     };
     if(req.query.status) {
         find.status = req.query.status;
+        // mặc địch query sẽ là ?status luôn
     }
 
-    const articles = await Article.find(find);
+    // Sort
+    const sort = {};
+
+    if(req.query.sortKey && req.query.sortValue) {
+        sort[req.query.sortKey] = req.query.sortValue
+        // còn sort[req.query.sortKey] thì query sẽ thay đổi theo cặp key-value truyền vô
+    }
+    // End Sort
+
+    const articles = await Article.find(find).sort(sort);
     
     res.json(articles);
 };
